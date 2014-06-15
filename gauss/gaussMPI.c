7bf3c9dcea28b7e42bbc4f22325e478dd1772533
macover <zhzhliyu@163.com>
2014-06-15 19:09:38 +0800

    change GaussMPI.c

--- gauss/gaussMPI.c
+++ gauss/gaussMPI.c
@@ -10,31 +10,14 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <math.h>
-#define a(x,y) a[x*M+y]
-#define b(x) b[x]
-#define A(x,y) A[x*M+y]
-#define B(x) B[x]  
+
+ 
 
 int M,N,m;
 float *A,*B;
 double starttime,time1,time2;
 int my_rank,p,l;
 MPI_Status status;
-
-void fatal(char *message)
-{
-    printf("%s\n",message);
-    exit(1);
-}
-
-
-void Environment_Finalize(float *a,float *b,float *x,float *f)
-{
-    free(a);
-    free(b);
-    free(x);
-    free(f);
-}
 
 
 int main(int argc, char **argv)
@@ -54,8 +37,7 @@ int main(int argc, char **argv)
 
     if (my_rank==0)
     {
-        starttime=MPI_Wtime();
-
+        starttime=MPI_Wtime(); 
         fdA=fopen("Gauss.data","r");
         fscanf(fdA,"%d %d", &M, &N);
         if (M != N-1)
@@ -78,35 +60,35 @@ int main(int argc, char **argv)
         fclose(fdA);
     }
 
-    MPI_Bcast(&M,1,MPI_INT,0,MPI_COMM_WORLD);     /* 0号处理机将M广播给所有处理机 */
+    MPI_Bcast(&M,1,MPI_INT,0,MPI_COMM_WORLD);    
     m=M/p;
     if (M%p!=0) m++;
 
-    f=(float*)malloc(sizeof(float)*(M+1));        /* 各处理机为主行元素建立发送和接收缓冲区(M+1) */
-    a=(float*)malloc(sizeof(float)*m*M);          /* 分配至各处理机的子矩阵大小为m*M */
-    b=(float*)malloc(sizeof(float)*m);            /* 分配至各处理机的子向量大小为m */
+    f=(float*)malloc(sizeof(float)*(M+1));        
+    a=(float*)malloc(sizeof(float)*m*M);          
+    b=(float*)malloc(sizeof(float)*m);            
     sum=(float*)malloc(sizeof(float)*m);
     x=(float*)malloc(sizeof(float)*M);
     shift=(int*)malloc(sizeof(int)*M);
 
     if (a==NULL||b==NULL||f==NULL||sum==NULL||x==NULL||shift==NULL)
-        fatal("allocate error\n");
+	{
+	    printf("allocate error\n");
+		exit(1);
+	} 
 
     for(i=0;i<M;i++)
         shift[i]=i;
 
-    /*
-     0号处理器采用行交叉划分将矩阵A划分为大小为m*M的p块子矩阵,将B划分为大小
-     为m的p块子向量，依次发送给1至p-1号处理机
-    */
+
     if (my_rank==0)
     {
         for(i=0;i<m;i++)
             for(j=0;j<M;j++)
-                a(i,j)=A(i*p,j);
+                a[i*M+j]=A[i*p*M+j];
 
         for(i=0;i<m;i++)
-            b(i)=B(i*p);
+            b[i]=B[i*p];
     }
 
     if (my_rank==0)
@@ -117,44 +99,44 @@ int main(int argc, char **argv)
             i1=i%p;
             i2=i/p+1;
 
-            MPI_Send(&A(i,0),M,MPI_FLOAT,i1,i2,MPI_COMM_WORLD);
-            MPI_Send(&B(i),1,MPI_FLOAT,i1,i2,MPI_COMM_WORLD);
+            MPI_Send(&A[i*M],M,MPI_FLOAT,i1,i2,MPI_COMM_WORLD);
+            MPI_Send(&B[i],1,MPI_FLOAT,i1,i2,MPI_COMM_WORLD);
         }
     }                                             /*  my_rank==0 */
     else                                          /*  my_rank !=0 */
     {
         for(i=0;i<m;i++)
         {
-            MPI_Recv(&a(i,0),M,MPI_FLOAT,0,i+1,MPI_COMM_WORLD,&status);
-            MPI_Recv(&b(i),1,MPI_FLOAT,0,i+1,MPI_COMM_WORLD,&status);
+            MPI_Recv(&a[i*M],M,MPI_FLOAT,0,i+1,MPI_COMM_WORLD,&status);
+            MPI_Recv(&b[i],1,MPI_FLOAT,0,i+1,MPI_COMM_WORLD,&status);
         }
     }
 
-    time1=MPI_Wtime();                            /* 开始计时 */
+    time1=MPI_Wtime();                           
 
-    for(i=0;i<m;i++)                              /* 消去 */
+    for(i=0;i<m;i++)                            
         for(j=0;j<p;j++)
     {
-        if (my_rank==j)                           /* j号处理机负责广播主行元素 */
+        if (my_rank==j)                         
         {
-            v=i*p+j;                              /* 主元素在原系数矩阵A中的行号和列号为v */
-            lmax=a(i,v);
+            v=i*p+j;                           
+            lmax=a[i*M+v];
             l=v;
 
-            for(k=v+1;k<M;k++)                    /* 在同行的元素中找最大元，并确定最大元所在的列l */
-                if (fabs(a(i,k))>lmax)
+            for(k=v+1;k<M;k++)                   
+                if (fabs(a[i*M+k])>lmax)
             {
-                lmax=a(i,k);
+                lmax=a[i*M+k];
                 l=k;
             }
 
-            if (l!=v)                             /* 列交换 */
+            if (l!=v)                           
             {
                 for(t=0;t<m;t++)
                 {
-                    temp=a(t,v);
-                    a(t,v)=a(t,l);
-                    a(t,l)=temp;
+                    temp=a[t*M+v];
+                    a[t*M+v]=a[t*M+l];
+                    a[t*M+l]=temp;
                 }
 
                 tem=shift[v];
@@ -162,18 +144,16 @@ int main(int argc, char **argv)
                 shift[l]=tem;
             }
 
-            for(k=v+1;k<M;k++)                    /* 归一化 */
-                a(i,k)=a(i,k)/a(i,v);
+            for(k=v+1;k<M;k++)                 
+                a[i*M+k]=a[i*M+k]/a[i*M+v];
 
-            b(i)=b(i)/a(i,v);
-            a(i,v)=1;
+            b[i]=b[i]/a[i*M+v];
+            a[i*M+v]=1;
 
             for(k=v+1;k<M;k++)
-                f[k]=a(i,k);
-            f[M]=b(i);
-			 /* 发送归一化后的主行 */
+                f[k]=a[i*M+k];
+            f[M]=b[i];
             MPI_Bcast(&f[0],M+1,MPI_FLOAT,my_rank,MPI_COMM_WORLD); 
-            /* 发送主行中主元素所在的列号 */
 			MPI_Bcast(&l,1,MPI_INT,my_rank,MPI_COMM_WORLD);
         }
         else
@@ -186,9 +166,9 @@ int main(int argc, char **argv)
             {
                 for(t=0;t<m;t++)
                 {
-                    temp=a(t,v);
-                    a(t,v)=a(t,l);
-                    a(t,l)=temp;
+                    temp=a[t*M+v];
+                    a[t*M+v]=a[t*M+l];
+                    a[t*M+l]=temp;
                 }
 
                 tem=shift[v];
@@ -201,32 +181,31 @@ int main(int argc, char **argv)
             for(k=i+1;k<m;k++)
         {
             for(w=v+1;w<M;w++)
-                a(k,w)=a(k,w)-f[w]*a(k,v);
-            b(k)=b(k)-f[M]*a(k,v);
+                a[k*M+w]=a[k*M+w]-f[w]*a[k*M+v];
+            b[k]=b[k]-f[M]*a[k*M+v];
         }
 
         if (my_rank>j)
             for(k=i;k<m;k++)
         {
             for(w=v+1;w<M;w++)
-                a(k,w)=a(k,w)-f[w]*a(k,v);
-            b(k)=b(k)-f[M]*a(k,v);
+                a[k*M+w]=a[k*M+w]-f[w]*a[k*M+v];
+            b[k]=b[k]-f[M]*a[k*M+v];
         }
     }                                             
 
     for(i=0;i<m;i++)
         sum[i]=0.0;
-	 /* 回代 */
     for(i=m-1;i>=0;i--)                            
         for(j=p-1;j>=0;j--)
             if (my_rank==j)
             {
-                x[i*p+j]=(b(i)-sum[i])/a(i,i*p+j);
+                x[i*p+j]=(b[i]-sum[i])/a[i*M+i*p+j];
 
                 MPI_Bcast(&x[i*p+j],1,MPI_FLOAT,my_rank,MPI_COMM_WORLD);
 
                 for(k=0;k<i;k++)
-                    sum[k]=sum[k]+a(k,i*p+j)*x[i*p+j];
+                    sum[k]=sum[k]+a[k*M+i*p+j]*x[i*p+j];
             }
             else
             {
@@ -234,11 +213,11 @@ int main(int argc, char **argv)
 
         if (my_rank>j)
             for(k=0;k<i;k++)
-                sum[k]=sum[k]+a(k,i*p+j)*x[i*p+j];
+                sum[k]=sum[k]+a[k*M+i*p+j]*x[i*p+j];
 
         if (my_rank<j)
             for(k=0;k<=i;k++)
-                sum[k]=sum[k]+a(k,i*p+j)*x[i*p+j];
+                sum[k]=sum[k]+a[k*M+i*p+j]*x[i*p+j];
     }
 
     if (my_rank!=0)
@@ -255,8 +234,8 @@ int main(int argc, char **argv)
         printf("%d\t%d\n", M, N);
         for(i=0;i<M;i++)
         {
-            for(j=0;j<M;j++) printf("%f\t",A(i,j));
-            printf("%f\n",B(i));
+            for(j=0;j<M;j++) printf("%f\t",A[i*M+j]);
+            printf("%f\n",B[i]);
         }
         printf("\nOutput of solution\n");
         for(k=0;k<M;k++)
@@ -275,7 +254,10 @@ int main(int argc, char **argv)
 		printf("\nThis time gaussMPI.exe runtime=%lf\n\n",time2-time1); 
     }
 
-    MPI_Finalize();
-    Environment_Finalize(a,b,x,f);
+    MPI_Finalize(); 
+	free(a);
+    free(b);
+    free(x);
+    free(f);
     return(0);
 }
